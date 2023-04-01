90d62363e0a1eca386fd0cb4cc85aa6a7f094a55
fromddy <ddy_mainland@163.com>
2023-04-01 21:52:27 +0800

    Evil Wormhole

---
+++ ganache/bash.sh
@@ -0,0 +1,6 @@
+npx ganache --database.dbPath "./data/save/" \
+ --wallet.mnemonic "change typical hire slam amateur loan grid fix drama electric seed label" \
+  --wallet.defaultBalance 200000 \
+  --wallet.totalAccounts 20 \
+ 
+
